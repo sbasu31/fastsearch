807e4201b27d84dcc00107f510193c93fd0bbc52
shrutib <shrutib@blr-shrutib.vmware.com>
2014-01-24 17:48:06 +0530

    This is new test

--- src/com/test/TestRange.java
+++ src/com/test/TestRange.java
@@ -3,6 +3,10 @@ package com.test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TestRange {
 	public static void main(String[]args){
@@ -13,22 +17,40 @@ public class TestRange {
 			RandomAccessFile raf = new RandomAccessFile("E:\\shruti\\work\\Filed Bugs\\vpxd-1.log", "r");
 			int totalLength = (int) raf.length();
 			System.out.println("Total Length of File :" +totalLength);
-			
+			List<List<String>> result = new ArrayList<List<String>>();
+			boolean started = false;
 			int lineNo = 1;
+			int occurance = 0;
 			while(true){
 				String line = raf.readLine();
 				if(line == null)
 					break;
-				if(line.startsWith(rangeStart))
-				{
+				List<String> list =null;
+				if(line.startsWith(rangeStart) && !started ){
+					occurance++;
+					list = new ArrayList<String>();
+					started = true;
+				}
+				if(started){
+					list.add("[ "+lineNo+" ] "+line);
+					
+				}
+				if(line.startsWith(rangeEnd)){
+					started = false;
+					result.add(list);
 					
 				}
 				lineNo++;
+			}
+			int occurance1;
+			for(List<String>list : result){
+				
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 
 }
